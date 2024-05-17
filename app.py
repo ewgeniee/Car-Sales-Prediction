ee0cf180e0d3ad53ec2a491455ffbc811467b300
ewgeniee <kewgenie@gmail.com>
2024-05-17 18:15:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def main():
     make_name = st.selectbox("Make Name", label_encoders["make_name"].classes_)
     model_name = st.selectbox("Model Name", label_encoders["model_name"].classes_)
     mileage = st.slider("Mileage", min_value=0, max_value=1000000, value=0, step=1000, format='%d')
-    year = st.slider("Year",1950, 2024, 1)
+    year = st.slider("Year", min_value=1950, max_value=2024, value=1950,step=1, format='%d')
     fuel_type = st.selectbox("Fuel Type", label_encoders["fuel_type"].classes_)
     transmission = st.selectbox("Transmission", label_encoders["transmission"].classes_)
     body_type = st.selectbox("Body Type", label_encoders["body_type"].classes_)
